9638d65490da4d469736726b3bf84a6511562995
FlorentinoGB <59677319+FlorentinoGB@users.noreply.github.com>
2020-11-11 18:49:23 -0600

    Class 2. Adding header module view, style, top bar and header menu

---
+++ frontend/view/modules/header.php
@@ -0,0 +1,138 @@
+<div class="container-fluid topBar" id="top">
+    <div class="container">
+        <div class="row">
+            <!-- Social networks -->
+            <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+                <ul>
+                    <li>
+                        <a href="https://facebook.com" target="_blank">
+                            <i class="fa fa-facebook"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://youtube.com" target="_blank">
+                            <i class="fa fa-youtube"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://instagram.com" target="_blank">
+                            <i class="fa fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com" target="_blank">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Registry -->
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 social">
+                <ul>
+                    <li>
+                        ><a href="#signInModal" data-toggle="model">Sign in</a>
+                    </li>
+                    <li>|</li>
+                    <li><a href="#createAccountModal" data-toggle="model">Create an account</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- header -->
+<header class="container-fluid">
+    <div class="container">
+        <div class="row" id="header">
+            <!-- logo -->
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logo">
+                <a href="#">
+                    <img src="http://localhost/univa-ecommerce/backend/view/img/template/logo.png" class="img-responsive" alt="UNIVA-eCommerce">
+                </a>
+            </div>
+            <!-- Categories & Search -->
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+                <!-- Categories button -->
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backgroundColor" id="buttonCategories">    
+                    <p>Categories
+                        <span class="pull-right">
+                            <i class="fa fa-bars" aria-hidden="true"></i>
+                        </span>
+                    </p>
+                </div>
+                <!-- Search -->
+                <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="search">
+                    <input type="search" name="search" class="form-control" placeholder="Search...">
+                    <span class="input-group-btn">
+                        <a href="#">
+                            <button class="btn btn-default backgroundColor" type="submit">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                            </button>
+                        </a>
+                    </span>
+                </div>
+            </div>
+            <! -- Shopping car -->
+            <div class ="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="shoppingCar">
+                <a href="#">
+                    <button class="btn btn-default pull-left backgroundColor">
+                        <i class="fa fa-shopping-cart"></i>
+                    </button>
+                </a>
+            </div>
+        </div>
+        <div class="col-xs12 backgroundColor" id="categories">
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories"Lorem ipsum></a>
+                </h4>
+                <hr>
+                <ul>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories"Lorem ipsum></a>
+                </h4>
+                <hr>
+                <ul>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories"Lorem ipsum></a>
+                </h4>
+                <hr>
+                <ul>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+                <h4>
+                    <a href="#" class="pixelCategories"Lorem ipsum></a>
+                </h4>
+                <hr>
+                <ul>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                    <li><a href="http://" target="_blank" class="pixelSubCategories">Lorem ipsum</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
--- frontend/view/template.php
+++ frontend/view/template.php
@@ -3,9 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="UNIVA - eCommerce">
+    <meta name="description" content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto molestias iste rem quibusdam cum iusto ducimus tempora amet tempore, adipisci explicabo quisquam voluptates temporibus cumque, facilis a. Necessitatibus, veniam id?">
+    <meta name="keywords" content="Lorem, ipsum, dolor, sit, amet, consectetur, adipisicing, elit">
     <title>UNIVA - ecommerce</title>
+    <link rel="stylesheet" href="view/CSS/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="view/CSS/plugins/font-awesome.min.css">
+    <script src="view/js/plugins/jquery.min.js"></script>
+    <script src="view/js/plugins/bootstrap.min.js"></script>
 </head>
 <body>
-    <h1>UNIVA - eCommerce - Beltran - FE</h1>
+    <?php
+
+        include "modules/header.php";
+
+    ?>
 </body>
 </html>
